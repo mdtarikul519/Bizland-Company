2096a57cf9cd7a4c65b0ac0ae57c8ca9f0fcd3e2
mdtarikul519 <tarikulmd519@gmail.com>
2022-06-27 15:27:15 +0600

    change

--- app/Http/Controllers/backend/IconController.php
+++ app/Http/Controllers/backend/IconController.php
@@ -2,66 +2,69 @@
 
 namespace App\Http\Controllers\backend;
 
-use Auth;
 use App\Models\Icon;
+use App\Models\About;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class IconController extends Controller
-
+{
+    public function view()
     {
-        public function view()
-        {
-            $allData = Icon::all();
-            return view('backend.icon.view-icon', compact('allData'));
+        $allData = Icon::all();
+        return view('backend.icon.view-icon', compact('allData'));
+    }
+      
+    public function add()
+    {
+        return view('backend.icon.add-icon');
+    }
+      
+    public function store(Request $request)
+    {
+        $data = new Icon();
+        $data->icon = $request->icon;
+        $data->name = $request->name;
+        $data->number = $request->number;
+        $data->save();
+        return redirect()->route('icon.view')->with('success', 'Data inserted successfully');
+    }
+      
+    public function edit($id)
+    {
+        $editdata = Icon::find($id);
+        return view('backend.icon.edit-icon', compact('editdata'));
+    }
+      
+    public function update(Request $request, $id)
+    {
+        $data = About::find($id);
+        $data->short_title = $request->short_title;
+        $data->long_title = $request->long_title;
+        $data->description = $request->description;
+        $data->updated_by = Auth::user()->id;
+        if ($request->file('image')) {
+            $file = $request->file('image');
+            @unlink(public_path('upload/about_images/'.$data->image));
+            $filename =date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/about_images'), $filename);
+            $data['image']= $filename;
         }
+        $data->icon = $request->icon;
+        $data->save();
+        return redirect()->route('about.view')->with('success', 'Data updated successfully');
+    }
       
-        public function add()
-        {
-            return view('backend.icon.add-icon');
+      
+    public function delete($id)
+    {
+        $slider = Slider::find($id);
+        if (file_exists('upload/slider_images/' . $slider->image) and ! empty($slider->image)) {
+            unlink('upload/slider_images/' . $slider->image);
         }
-      
-    public function store(Request $request){
-            $data = new Icon();
-            $data->icon = $request->icon;
-            $data->name = $request->name;
-            $data->number = $request->number;
-            $data->save();
-            return redirect()->route('icon.view')->with('success','Data inserted successfully');
-        }
-      
-    public function edit($id){
-            $editdata = Icon::find($id);
-            return view('backend.icon.edit-icon',compact('editdata'));
-           }
-      
-     public function update(Request $request, $id){
-          $data = About::find($id);
-          $data->short_title = $request->short_title;
-          $data->long_title = $request->long_title;
-          $data->description = $request->description;
-          $data->updated_by = Auth::user()->id;
-      if($request->file('image')) {
-          $file = $request->file('image');
-          @unlink(public_path('upload/about_images/'.$data->image));
-          $filename =date('YmdHi').$file->getClientOriginalName();
-          $file->move(public_path('upload/about_images'), $filename);
-          $data['image']= $filename;
-       }
-       $data->icon = $request->icon;
-     $data->save();
-     return redirect()->route('about.view')->with('success','Data updated successfully');
-    
-         }
-      
-      
-        public function delete($id)
-        {
-            $slider = Slider::find($id);
-          if (file_exists('upload/slider_images/' . $slider->image) AND ! empty($slider->image)) {
-              unlink('upload/slider_images/' . $slider->image);
-           } 
-           $slider->delete(); 
-            return redirect()->route('slider.view')->with('success','Data deleted successfully');
-         }
-        }
+        $slider->delete();
+        return redirect()->route('slider.view')->with('success', 'Data deleted successfully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,60 +24,59 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::prefix('users')->group(function(){
-    Route::get('/view','App\Http\Controllers\backend\userController@view')->name('user.view');
-    Route::get('/add','App\Http\Controllers\backend\userController@add')->name('user.add');
-    Route::post('/store','App\Http\Controllers\backend\userController@store')->name('user.store');
-    Route::get('/edit/{id}','App\Http\Controllers\backend\userController@edit')->name('user.edit');
-    Route::post('/update/{id}','App\Http\Controllers\backend\userController@update')->name('user.update');
-    Route::get('/delete/{id}','App\Http\Controllers\backend\userController@delete')->name('user.delete');
-    });
+Route::prefix('users')->group(function () {
+    Route::get('/view', 'App\Http\Controllers\backend\userController@view')->name('user.view');
+    Route::get('/add', 'App\Http\Controllers\backend\userController@add')->name('user.add');
+    Route::post('/store', 'App\Http\Controllers\backend\userController@store')->name('user.store');
+    Route::get('/edit/{id}', 'App\Http\Controllers\backend\userController@edit')->name('user.edit');
+    Route::post('/update/{id}', 'App\Http\Controllers\backend\userController@update')->name('user.update');
+    Route::get('/delete/{id}', 'App\Http\Controllers\backend\userController@delete')->name('user.delete');
+});
 
 
-Route::prefix('profiles')->group(function(){
-    Route::get('/view','App\Http\Controllers\backend\ProfileController@view')->name('profiles.view');
-    Route::get('/edit','App\Http\Controllers\backend\ProfileController@edit')->name('profiles.edit');
-    Route::post('/update','App\Http\Controllers\backend\ProfileController@update')->name('profiles.update');
-    Route::get('/passowrd/view','App\Http\Controllers\backend\ProfileController@passwordView')->name('profiles.passowrd.view');
-    Route::post('/passowrd/update','App\Http\Controllers\backend\ProfileController@passwordUpdate')->name('profiles.passowrd.update');
- 
-    });
+Route::prefix('profiles')->group(function () {
+    Route::get('/view', 'App\Http\Controllers\backend\ProfileController@view')->name('profiles.view');
+    Route::get('/edit', 'App\Http\Controllers\backend\ProfileController@edit')->name('profiles.edit');
+    Route::post('/update', 'App\Http\Controllers\backend\ProfileController@update')->name('profiles.update');
+    Route::get('/passowrd/view', 'App\Http\Controllers\backend\ProfileController@passwordView')->name('profiles.passowrd.view');
+    Route::post('/passowrd/update', 'App\Http\Controllers\backend\ProfileController@passwordUpdate')->name('profiles.passowrd.update');
+});
 
-Route::prefix('logo')->group(function(){
-    Route::get('/view','App\Http\Controllers\backend\logoController@view')->name('logo.view');
-    Route::get('/add','App\Http\Controllers\backend\logoController@add')->name('logo.add');
-    Route::post('/store','App\Http\Controllers\backend\logoController@store')->name('logo.store');
-    Route::get('/edit/{id}','App\Http\Controllers\backend\logoController@edit')->name('logo.edit');
-    Route::post('/update/{id}','App\Http\Controllers\backend\logoController@update')->name('logo.update');
-    Route::get('/delete/{id}','App\Http\Controllers\backend\logoController@delete')->name('logo.delete');
-    });
+Route::prefix('logo')->group(function () {
+    Route::get('/view', 'App\Http\Controllers\backend\LogoController@view')->name('logo.view');
+    Route::get('/add', 'App\Http\Controllers\backend\LogoController@add')->name('logo.add');
+    Route::post('/store', 'App\Http\Controllers\backend\LogoController@store')->name('logo.store');
+    Route::get('/edit/{id}', 'App\Http\Controllers\backend\LogoController@edit')->name('logo.edit');
+    Route::post('/update/{id}', 'App\Http\Controllers\backend\LogoController@update')->name('logo.update');
+    Route::get('/delete/{id}', 'App\Http\Controllers\backend\LogoController@delete')->name('logo.delete');
+});
 
-Route::prefix('slider')->group(function(){
-    Route::get('/view','App\Http\Controllers\backend\sliderController@view')->name('slider.view');
-    Route::get('/add','App\Http\Controllers\backend\sliderController@add')->name('slider.add');
-    Route::post('/store','App\Http\Controllers\backend\sliderController@store')->name('slider.store');
-    Route::get('/edit/{id}','App\Http\Controllers\backend\sliderController@edit')->name('slider.edit');
-    Route::post('/update/{id}','App\Http\Controllers\backend\sliderController@update')->name('slider.update');
-    Route::get('/delete/{id}','App\Http\Controllers\backend\sliderController@delete')->name('slider.delete');
-    });
-
+Route::prefix('slider')->group(function () {
+    Route::get('/view', 'App\Http\Controllers\backend\SliderController@view')->name('slider.view');
+    Route::get('/add', 'App\Http\Controllers\backend\SliderController@add')->name('slider.add');
+    Route::post('/store', 'App\Http\Controllers\backend\SliderController@store')->name('slider.store');
+    Route::get('/edit/{id}', 'App\Http\Controllers\backend\SliderController@edit')->name('slider.edit');
+    Route::post('/update/{id}', 'App\Http\Controllers\backend\SliderController@update')->name('slider.update');
+    Route::get('/delete/{id}', 'App\Http\Controllers\backend\SliderController@delete')->name('slider.delete');
+});
 
 
-    Route::prefix('about')->group(function(){
-        Route::get('/view','App\Http\Controllers\backend\aboutController@view')->name('about.view');
-        Route::get('/add','App\Http\Controllers\backend\aboutController@add')->name('about.add');
-        Route::post('/store','App\Http\Controllers\backend\aboutController@store')->name('about.store');
-        Route::get('/edit/{id}','App\Http\Controllers\backend\aboutController@edit')->name('about.edit');
-        Route::post('/update/{id}','App\Http\Controllers\backend\aboutController@update')->name('about.update');
-        Route::get('/delete/{id}','App\Http\Controllers\backend\aboutController@delete')->name('about.delete');
-        });
-    
 
-        Route::prefix('icon')->group(function(){
-            Route::get('/view','App\Http\Controllers\backend\IAconController@view')->name('icon.view');
-            Route::get('/add','App\Http\Controllers\backend\IAconController@add')->name('icon.add');
-            Route::post('/store','App\Http\Controllers\backend\IAconController@store')->name('icon.store');
-            Route::get('/edit/{id}','App\Http\Controllers\backend\IAconController@edit')->name('icon.edit');
-            Route::post('/update/{id}','App\Http\Controllers\backend\IAconController@update')->name('icon.update');
-            Route::get('/delete/{id}','App\Http\Controllers\backend\IAconController@delete')->name('icon.delete');
-            });
+Route::prefix('about')->group(function () {
+    Route::get('/view', 'App\Http\Controllers\backend\AboutController@view')->name('about.view');
+    Route::get('/add', 'App\Http\Controllers\backend\AboutController@add')->name('about.add');
+    Route::post('/store', 'App\Http\Controllers\backend\AboutController@store')->name('about.store');
+    Route::get('/edit/{id}', 'App\Http\Controllers\backend\AboutController@edit')->name('about.edit');
+    Route::post('/update/{id}', 'App\Http\Controllers\backend\AboutController@update')->name('about.update');
+    Route::get('/delete/{id}', 'App\Http\Controllers\backend\AboutController@delete')->name('about.delete');
+});
+
+
+Route::prefix('icon')->group(function () {
+    Route::get('/view', 'App\Http\Controllers\backend\IconController@view')->name('icon.view');
+    Route::get('/add', 'App\Http\Controllers\backend\IconController@add')->name('icon.add');
+    Route::post('/store', 'App\Http\Controllers\backend\IconController@store')->name('icon.store');
+    Route::get('/edit/{id}', 'App\Http\Controllers\backend\IconController@edit')->name('icon.edit');
+    Route::post('/update/{id}', 'App\Http\Controllers\backend\IconController@update')->name('icon.update');
+    Route::get('/delete/{id}', 'App\Http\Controllers\backend\IconController@delete')->name('icon.delete');
+});
